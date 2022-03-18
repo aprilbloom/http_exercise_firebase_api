69a3cf964cb68b1a7aeaf02226b3493ed08bdc8c
lleonor <leahaprilleonor@gmail.com>
2022-03-18 17:52:34 -0500

    Firebase Api - Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './post.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'http_exercise';
+
+  loadedPosts: Post[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: {title: string, content: string}){
+    this.http
+    .post< {name: string} >(
+        'https://ng-complete-guide-68ffd-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData)
+      });
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get<{ [key:string]: Post } >('https://ng-complete-guide-68ffd-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData => {
+        const postsArray = [];
+        for(const key in responseData){
+            if(responseData.hasOwnProperty(key)){
+            //postsArray.push({ ...JSON.parse(JSON.stringify(responseData))[key], id: key})
+            postsArray.push({ ...responseData[key], id: key})
+          }
+        }
+        return postsArray;
+      })
+      )
+      .subscribe(posts => {
+        this.loadedPosts = posts;
+      });
+  }
 }
