4622b265b1fd20659955d95a3b854766bdcea8c6
lleonor <leahaprilleonor@gmail.com>
2022-03-19 09:54:17 -0500

    Interact with the response through interceptor, ex log response body

--- src/auth-interceptor.service.ts
+++ src/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -10,6 +10,13 @@ export class AuthInterceptorService implements HttpInterceptor {
         headers: req.headers.append('Auth', 'xyz')
       });
 
-      return next.handle(modifiedReq);
+      return next.handle(modifiedReq).pipe(
+        tap(event => {
+          if(event.type == HttpEventType.Response){
+            console.log('Response arrived, body data: ');
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
